63960e3300f21e319966365e703f883de5f497df
emilhuseynvh <emilhuseynvh@gmail.com>
2025-02-12 15:32:04 +0400

    typeorm added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
 import config from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import DataSource from './config/database';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(DataSource.options),
     JwtModule.register({
       global: true,
       secret: config.jwtSecret,
