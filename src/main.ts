7d7fd6fed6ebeb45ac56351e3cd7a61cfe517c00
emilhuseynvh <emilhuseynvh@gmail.com>
2025-03-21 00:54:29 +0400

    updated

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'localhost'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
